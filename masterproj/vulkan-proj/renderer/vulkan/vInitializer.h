d2d1e115b1723c9823f687b94e20a8f60e9bd6ce
jitsuc4 <jitsuc4@gmail.com>
2018-10-02 20:27:22 +0200

    mipmapping test

--- masterproj/vulkan-proj/renderer/vulkan/vInitializer.h
+++ masterproj/vulkan-proj/renderer/vulkan/vInitializer.h
@@ -232,21 +232,23 @@ private:
 	std::vector<VkDescriptorSet> descriptorSets;
 
 	/* Images */
+	uint32_t m_mipLevels;
 	VkImage textureImage;
 	VkDeviceMemory textureImageMemory;
 
-	void createImage(uint32_t width, uint32_t height, VkFormat format, VkImageTiling tiling, VkImageUsageFlags usage, VkMemoryPropertyFlags properties, VkImage& image, VkDeviceMemory& imageMemory);
+	void createImage(uint32_t width, uint32_t height, uint32_t mipLevels, VkFormat format, VkImageTiling tiling, VkImageUsageFlags usage, VkMemoryPropertyFlags properties, VkImage& image, VkDeviceMemory& imageMemory);
+	void generateMipmaps(VkImage image, VkFormat imageFormat, int32_t texWidth, int32_t texHeight, uint32_t mipLevels);
 	
 	VkCommandBuffer beginSingleTimeCommands();
 	void endSingleTimeCommands(VkCommandBuffer commandBuffer);
-	void transitionImageLayout(VkImage image, VkFormat format, VkImageLayout oldLayout, VkImageLayout newLayout);
+	void transitionImageLayout(VkImage image, VkFormat format, VkImageLayout oldLayout, VkImageLayout newLayout, uint32_t mipLevels);
 	void copyBufferToImage(VkBuffer buffer, VkImage image, uint32_t width, uint32_t height);
 
 	/* Image view and sampler */
 	VkImageView textureImageView;
 	VkSampler textureSampler;
 
-	VkImageView createImageView(VkImage image, VkFormat format, VkImageAspectFlags aspectFlags);
+	VkImageView createImageView(VkImage image, VkFormat format, VkImageAspectFlags aspectFlags, uint32_t mipLevels);
 
 	/* Depth buffering */
 	VkImage depthImage;
